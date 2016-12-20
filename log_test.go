d74c0d0057b19205a81a005aff3cd8f711d416ea
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2016-12-20 16:15:46 -0200

    Fix tests.

--- log_test.go
+++ log_test.go
@@ -2,14 +2,9 @@ package log
 
 import (
 	"testing"
-
-	"github.com/eventials/golog"
-	"github.com/eventials/golog/console"
 )
 
 func TestConsoleLog(t *testing.T) {
-	logger := consolelog.NewConsoleLogger("")
-	log.AddLogger(logger)
-	log.Info("Console message")
-	log.Info("Console message with parameters: %s %d", "test", 100)
+	Info("Console message")
+	Info("Console message with parameters: %s %d", "test", 100)
 }
